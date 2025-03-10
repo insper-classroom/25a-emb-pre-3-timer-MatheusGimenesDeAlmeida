08c6ed924631e224362b63dd9ce6d6d924ba0659
MatheusGimenesDeAlmeida <mthga500@gmail.com>
2025-03-10 13:28:43 -0300

    ex1

--- exe1/main.c
+++ exe1/main.c
@@ -5,20 +5,21 @@
 const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
-volatile int flag_f_r = 0;
-volatile int flag_tempo = 0;
-volatile int timer_ativo = 0;  
 
-repeating_timer_t timer_0;
+volatile int flag_botao = 0;
+volatile int flag_timer = 0;
+
+repeating_timer_t timer;
 
 void btn_callback(uint gpio, uint32_t events) {
-    if (events == 0x4) { 
-        flag_f_r = 1;
+    if (events == 0x4) { // fall edge
+        flag_botao = 1;
+    } else if (events == 0x8) { // rise edge
     }
 }
 
-bool timer_0_callback(repeating_timer_t *rt) {
-    flag_tempo = 1;
+bool timer_callback(repeating_timer_t *rt) {
+    flag_timer = 1;
     return true; 
 }
 
@@ -26,35 +27,27 @@ int main() {
     stdio_init_all();
 
     gpio_init(LED_PIN_R);
-    gpio_set_dir(LED_PIN_R, GPIO_OUT);
+    gpio_set_dir(LED_PIN_R, GPIO_OUT);  
 
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
     gpio_pull_up(BTN_PIN_R);
 
-    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
-                                       &btn_callback);
-
-    add_repeating_timer_ms(500, timer_0_callback, NULL, &timer_0); 
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, btn_callback);
 
     while (true) {
 
-        if (flag_f_r) {
-            timer_ativo = !timer_ativo; 
-
-            if (timer_ativo == 0) {
-                gpio_put(LED_PIN_R, 0); 
-            }
-
-            flag_f_r = 0;
+        if (flag_botao) {
+            flag_botao = 0;
+            gpio_put(LED_PIN_R, 1);
+            add_repeating_timer_ms(500, timer_callback, NULL, &timer);
         }
 
-        if (flag_tempo) {
-            if (timer_ativo) {
-                gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R)); 
-            }
-            flag_tempo = 0;
-        }
+        if (flag_timer) {
+            flag_timer = 0;
+            cancel_repeating_timer(&timer);
 
+            gpio_put(LED_PIN_R, 0);
+        }
     }
 }
